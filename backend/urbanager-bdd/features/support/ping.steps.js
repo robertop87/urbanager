1b82e07960eaaa99a975b3f79370e3c4255bedd4
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2018-04-17 00:17:45 -0400

    Installed request-promise and fix error test

--- backend/urbanager-bdd/features/support/ping.steps.js
+++ backend/urbanager-bdd/features/support/ping.steps.js
@@ -1,15 +1,19 @@
 const { Given, When, Then } = require('cucumber')
 const { expect } = require('chai')
 const request = require('request')
+const rp = require('request-promise')
 const HttpStatus = require('http-status')
 
 let httpResponseStatusCode = 0;
 When('make a get request to /ping-fake resource', function () {
   let me = this;
-  request
-    .get("http://localhost:8080/ping-fake")
+  request.get("http://localhost:8080/ping-fake")
     .on('error', function(error) {
       console.log("There was an error");
+      httpResponseStatusCode = 0;
+    })
+    .on('response', function(response) {
+      httpResponseStatusCode = response.statusCode;
     });
 });
 
@@ -17,17 +21,17 @@ Then('the response is not OK status', function () {
   expect(httpResponseStatusCode).to.not.eql(HttpStatus.OK);
 });
 
-When('make a get request to /ping', function () {
-  request.get("http://localhost:8080/api/ping")
+When('make a get request to /ping', async function () {
+  await rp.get("http://localhost:8080/api/ping")
     .on('response', function(response) {
-      if (response
-        && response.statusCode)
+      if (response && response.statusCode) {
         httpResponseStatusCode = response.statusCode;
+      }
     })
     .on('error', function(error) {
       if (error)
         console.log("There was an error in connecting with the server")
-    })
+    });
 });
 
 Then('the response is OK status', function () {
